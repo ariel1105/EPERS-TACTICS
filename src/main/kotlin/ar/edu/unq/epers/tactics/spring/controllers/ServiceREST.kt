a40b0eddfeabf5b2f60ccd01bbb4ff86656cc10e
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2023-04-28 13:03:09 -0300

    Delete src/main/kotlin/ar/edu/unq/epers/tactics/spring directory

--- src/main/kotlin/ar/edu/unq/epers/tactics/spring/controllers/ServiceREST.kt
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.epers.tactics.spring.controllers
-
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RestController
-
-
-@RestController
-@CrossOrigin(origins = ["*"])
-annotation class ServiceREST
